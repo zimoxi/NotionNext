c8cc0d8527a011afb01d23bb14275bcd4b39769e
zimoxi <65380596+zimoxi@users.noreply.github.com>
2024-07-10 16:10:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -279,7 +279,7 @@ const BLOG = {
   TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || '',
 
   // Chatbase 是否显示chatbase机器人 https://www.chatbase.co/
-  CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || ggh7HW-B2VriBnuhEcIqX,
+  CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || 'ggh7HW-B2VriBnuhEcIqX',
   // WebwhizAI 机器人 @see https://github.com/webwhiz-ai/webwhiz
   WEB_WHIZ_ENABLED: process.env.NEXT_PUBLIC_WEB_WHIZ_ENABLED || false, // 是否显示
   WEB_WHIZ_BASE_URL:
